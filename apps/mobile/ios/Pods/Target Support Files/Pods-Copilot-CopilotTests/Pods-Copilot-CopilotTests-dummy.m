8fe36885e59f8fbc8d588652078558ac406b5fc1
stefano-bassan <stefano_bassan@external.mckinsey.com>
2022-06-06 20:23:30 -0300

    create mobile app

---
+++ apps/mobile/ios/Pods/Target Support Files/Pods-Copilot-CopilotTests/Pods-Copilot-CopilotTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Copilot_CopilotTests : NSObject
+@end
+@implementation PodsDummy_Pods_Copilot_CopilotTests
+@end
---
+++ apps/mobile/ios/Pods/Target Support Files/Pods-Copilot/Pods-Copilot-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Copilot : NSObject
+@end
+@implementation PodsDummy_Pods_Copilot
+@end
